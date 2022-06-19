df20b93a544c2b7ab61fbb8bd0784b9195117550
Konrad Skalski <skalskikonrad@gmail.com>
2022-06-19 14:04:11 +0200

    Calculator testing

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,33 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania orpogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calcCheck = new Calculator();
+        int calcCheckAdd = calcCheck.add(5, 7);
+        if(calcCheckAdd == 12){
+            System.out.println("Numbers adding test OK");
+        } else {
+            System.out.println("Numbers adding test failed");
+        }
+        int calcChechSub = calcCheck.substract(14, 13);
+        if (calcChechSub == 1) {
+            System.out.println("Numbers subtraction test OK");
+        } else {
+            System.out.println("Numbers subtraction test failed");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,12 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    public int add(int a, int b) {
+        int resultAdd = a + b;
+        return resultAdd;
+    }
+    public int substract(int a, int b) {
+        int resultSub = a - b;
+        return resultSub;
+    }
+}
