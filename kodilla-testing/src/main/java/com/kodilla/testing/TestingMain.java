114c03c34c3a4c99e52f69712dffd4a396136bbb
Konrad Skalski <skalskikonrad@gmail.com>
2022-06-19 20:55:29 +0200

    Calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,11 +1,30 @@
 package com.kodilla.testing;
 
-import com.kodilla.testing.user.calculator.Calculator;
+import com.kodilla.testing.calculator.Calculator;
 import com.kodilla.testing.user.SimpleUser;
-
 public class TestingMain {
 
     public static void main(String[] args) {
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String result = simpleUser.getUsername();
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
 
+        Calculator calculator = new Calculator();
+        int addingResult = calculator.add(12, 11);
+        int substractResult = calculator.subtract(13, 12);
+        if (addingResult == 23) {
+            System.out.println("Adding test OK");
+        } else {
+            System.out.println("Adding test not OK");
+        }
+        if (substractResult == 1) {
+            System.out.println("Substraction test OK");
+        } else {
+            System.out.println("Substraction test not OK");
         }
     }
+}
--- kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -1,12 +1,13 @@
-package com.kodilla.testing.user.calculator;
+package com.kodilla.testing.calculator;
 
 public class Calculator {
     public int add(int a, int b) {
         int resultAdd = a + b;
         return resultAdd;
     }
-    public int substract(int a, int b) {
+    public int subtract(int a, int b) {
         int resultSub = a - b;
         return resultSub;
     }
+
 }
