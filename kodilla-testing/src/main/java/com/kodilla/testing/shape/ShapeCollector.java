adae59339f7a4db310618528a4c6e08a4c69509a
Konrad Skalski <skalskikonrad@gmail.com>
2022-06-21 21:03:09 +0200

    ShapeCollector

--- kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -3,9 +3,6 @@ package com.kodilla.testing.shape;
 import java.util.ArrayList;
 
 public class ShapeCollector {
-    Shape Circle;
-    Shape Square;
-    Shape Triangle;
     ArrayList<Shape> figures = new ArrayList<>();
     public void addFigure(Shape shape){
         figures.add(shape);
@@ -21,11 +18,16 @@ public class ShapeCollector {
     public Shape getFigure(int figureNumber){
         if(figureNumber >= 0 && figureNumber < figures.size()){
             return figures.get(figureNumber);
-        }
-        return null;
-    }
-    public String showFigures(){
-        //String figuresString = String.join(", ", figures);
-            return "hock";
+        }else {
+            return null;
         }
     }
+    public String showFigures() {
+            String figuresString = figures.toString();
+        return figuresString;
+    }
+
+    public int getFiguresQuantity() {
+        return figures.size();
+    }
+}
--- kodilla-testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
@@ -1,8 +1,83 @@
 package com.kodilla.testing.shape;
+import org.junit.jupiter.api.*;
 
-import org.junit.jupiter.api.DisplayName;
+import java.util.ArrayList;
 
 @DisplayName("TDD: Shape Collector Test Suite")
 public class ShapeCollectorTestSuite {
-    private static int testCountDown = 4;
+    private static int testNumber = 0;
+    @BeforeAll
+    public static void beforeAllTests() {
+        System.out.println("Tests are about to begin.");
+    }
+    @AfterAll
+    public static void afterAllTests() {
+        System.out.println("All tests are finished.");
+    }
+    @BeforeEach
+    public void beforeEveryTest() {
+        testNumber++;
+        System.out.println("Test number: " + testNumber);
+
+    }
+    @Test
+    void testAddFigure() {
+        //Given
+        Shape circle = new Circle("circle", 5);
+        ShapeCollector collector = new ShapeCollector();
+        //When
+        collector.addFigure(circle);
+
+        //Then
+        Assertions.assertEquals(1, collector.getFiguresQuantity());
+    }
+    @Test
+    void testRemoveFigure() {
+        //Given
+        Shape triangle = new Triangle("triangle",3.5, 4);
+        ShapeCollector collector = new ShapeCollector();
+        collector.addFigure(triangle);
+
+        //When
+        boolean result = collector.removeFigure(triangle);
+
+        //Then
+        Assertions.assertTrue(result);
+        Assertions.assertEquals(0, collector.getFiguresQuantity());
+    }
+    @Test
+    void testGetFigure() {
+        //Given
+        Shape square = new Square("triangle",7, 8);
+        ShapeCollector collector = new ShapeCollector();
+        collector.addFigure(square);
+
+        //When
+        Shape retrievedShape;
+        retrievedShape = collector.getFigure(0);
+        //Then
+        Assertions.assertEquals(square, retrievedShape);
+    }
+}
+
+    @Nested
+    @DisplayName("Tests for adding and showing ArrayList"
+    )
+    class arrayListShowTest{
+    @Test
+    void testShowFigures(){
+        //Given
+        Shape square = new Square("triangle",7, 8);
+        ShapeCollector collector = new ShapeCollector();
+        collector.addFigure(square);
+
+        //When
+        ArrayList<Shape> shapeList= new ArrayList<>();
+        shapeList.add(square);
+        String expectedFigures = shapeList.toString();
+        String retrievedFigures = collector.showFigures();
+
+        //Then
+        Assertions.assertEquals(expectedFigures, retrievedFigures);
+    }
 }
