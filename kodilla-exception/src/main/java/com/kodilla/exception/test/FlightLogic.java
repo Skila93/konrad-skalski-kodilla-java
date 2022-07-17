1431946db0480389ecb981fcf62ee42cc4e5c518
Konrad Skalski <skalskikonrad@gmail.com>
2022-07-17 22:28:27 +0200

    zad8.4 dodane

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightLogic.java
@@ -0,0 +1,57 @@
+package com.kodilla.exception.test;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FlightLogic {
+    public final void findFlight(Flight flight) throws RouteNotFoundException{
+        Map<String, Boolean> possibleFlights = new HashMap<>();
+        possibleFlights.put("Warsaw", true);
+        possibleFlights.put("New York", false);
+        possibleFlights.put("Rome", false);
+        possibleFlights.put("Berlin", true);
+        possibleFlights.put("London", true);
+        String arrivalAirport = flight.getArrivalAirport();
+        System.out.println("Departure airport: " + flight.getDepartureAirport());
+        System.out.println("Arrival Airport: " + arrivalAirport);
+        if (possibleFlights.get(arrivalAirport)){
+            System.out.println("The flight may take place");
+        }else{
+            System.out.println("Flight to chosen destination is not available");
+            throw new RouteNotFoundException();
+        }
+    }
+    public static void main(String[] args)
+    {
+        Flight berlinWarsaw = new Flight("Berlin", "Warsaw");
+        Flight newYorkBerlin = new Flight("New York", "Berlin");
+        Flight newYorkRome = new Flight("New York", "Rome");
+        Flight romeLondon = new Flight("Rome", "London");
+        Flight romeNewYork = new Flight("Rome", "New York");
+        FlightLogic flightLogic = new FlightLogic();
+        try {
+            flightLogic.findFlight(berlinWarsaw);
+        }catch(RouteNotFoundException e) {
+            System.out.println("Choose another flight");
+        }
+            try {
+            flightLogic.findFlight(newYorkBerlin);
+            }catch(RouteNotFoundException e){
+                System.out.println("Choose another flight");
+            }
+            try {
+            flightLogic.findFlight(newYorkRome);
+                }catch(RouteNotFoundException e){
+                System.out.println("Choose another flight");
+                }
+            try {
+            flightLogic.findFlight(romeLondon);
+            }catch(RouteNotFoundException e){
+                System.out.println("Choose another flight");
+            }
+            try {
+            flightLogic.findFlight(romeNewYork);
+            }catch(RouteNotFoundException e){
+            System.out.println("Choose another flight");
+        }
+    }
+}
