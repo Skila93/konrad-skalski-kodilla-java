21dbed565f94c1a90099d35acc6336687bc3f35b
Konrad Skalski <skalskikonrad@gmail.com>
2022-07-18 20:43:42 +0200

    Zad. 8.2 poprawione

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -6,28 +6,25 @@ import java.time.LocalTime;
 public class FirstChallenge {
 
     public double divide(double a, double b) throws ArithmeticException {
-
-        try {
-            if (b == 0){
+        if (b == 0) {
             throw new ArithmeticException();
-            }
-        } catch (ArithmeticException e) {
-            System.out.println("Exception found: " + e);
-        }
-        finally {
-            System.out.println("Date: " + LocalDate.now() + " ,time: " + LocalTime.now());
         }
         return a / b;
     }
-
     /**
      * This main can throw an ArithmeticException!!!
      * @param args
      */
     public static void main(String[] args) {
         FirstChallenge firstChallenge = new FirstChallenge();
-        double result = firstChallenge.divide(3, 0);
-
-        System.out.println(result);
+        try {
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+        } catch (ArithmeticException e){
+            System.out.println("Exception found: " + e);
+        }
+        finally {
+            System.out.println("Date: " + LocalDate.now() + " ,time: " + LocalTime.now());
+        }
     }
 }
